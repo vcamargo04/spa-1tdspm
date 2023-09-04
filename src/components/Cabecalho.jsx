02a1523068c414efbc5a4ffd93933c921aee2099
vcamargo04 <viniciusac2005@Hotmail.com>
2023-09-04 11:33:38 -0300

    Listagem dinamica, Recuperação via ID HOOK useParams().

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -5,7 +5,7 @@ export default function Cabecalho() {
   return (
     <>
       <header>
-        <h1>Vite + React / Coded By - PF0670</h1>
+        <h1>Vite + React / Coded By - RM-99494</h1>
         <ul>
           <li>
           <Link to="/">HOME - <H/></Link>
@@ -17,10 +17,10 @@ export default function Cabecalho() {
             <Link to="/editar/produtos/1">PRODUTO - 1</Link>
           </li>
           <li>
-            <Link to="/editar/produtos/1">PRODUTO - 2</Link>
+            <Link to="/editar/produtos/2">PRODUTO - 2</Link>
           </li>
           <li>
-            <Link to="/editar/produtos/1">PRODUTO - 3</Link>
+            <Link to="/editar/produtos/3">PRODUTO - 3</Link>
           </li>
         </ul>
       </header>
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,7 +1,47 @@
-
+import { Link } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
 
 export default function Produtos() {
+  document.title = "Produtos";
+
+  const estiloDasCelulas = {
+    border:"2px solid #ccc",
+    fontFamily:"Arial, Trebuchet, sans-serif"
+  }
+
   return (
-    <div>Produtos</div>
+    <div>
+      <h1>LISTA DE PRODUTOS</h1>
+
+      <table style={{border:"2px solid #ccc", borderCollapse:"collapse", margin:"auto"}}>
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>NOME</th>
+          <th>DESCRIÇÃO</th>
+          <th>PREÇO</th>
+          <th>EDITAR</th>       
+        </tr>
+        </thead>
+        <tbody>
+        {
+          ListaProdutos.map((item,indice)=>(
+            <tr key={indice}>
+                <td style={estiloDasCelulas}>{item.id}</td>
+                <td style={estiloDasCelulas}>{item.nome}</td>
+                <td style={estiloDasCelulas}>{item.desc}</td>
+                <td style={estiloDasCelulas}>{item.valor}</td>
+                <td style={estiloDasCelulas}> <Link to={`/editar/produtos/${item.id}`}>Editar</Link> </td>
+            </tr>
+          ))
+        }
+</tbody>
+<tfoot>
+  <tr>
+    <td colSpan={4}>PRODUTOS LINDOS</td>
+  </tr>
+</tfoot>
+      </table>
+    </div>
   )
 }
