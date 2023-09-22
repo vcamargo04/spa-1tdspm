f7331db32df9cadaf002414d0088673af2a955d5
vcamargo04 <viniciusac2005@hotmail.com>
2023-09-22 09:00:04 -0300

    Refatoramento do componente Produtos.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -3,41 +3,13 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import {GrFormEdit as Editar} from "react-icons/gr";
 import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
 import style from "./Produtos.module.css";
-import { useEffect, useState } from "react";
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  useEffect(()=>{
-    console.log("Este useEffect renderiza sempre! " + counter);
-  });
-  
-  const [counter, setCounter] = useState(0);
-
-  const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
-
-  useEffect(()=>{
-    console.log("Este useEffect renderiza apenas uma vez!");
-    setListaProdutosLocal(ListaProdutos);
-  },[ ]);
-
-  const [counter2, setCounter2] = useState(0)
-
-  useEffect(()=>{
-    console.log("Este useEffect renderiza sempre que o objeto/variável ou elemento que está sendo monitorado no array de dependências sofra alguma atualização.");
-  },[counter2]);
-
-
-
   return ( 
     <div>
       <h1>LISTA DE PRODUTOS</h1>
-      <div>
-        <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
-      </div>
-      <div>
-        <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
-      </div>
       <table className={style.tblEstilo}>
         <thead>
         <tr>
@@ -50,7 +22,7 @@ export default function Produtos() {
         </thead>
         <tbody>
         {
-          listaProdutosLocal.map((item,indice)=>(
+          ListaProdutos.map((item,indice)=>(
             <tr key={indice} className={style.tblLine}>
                 <td>{item.id}</td>
                 <td>{item.nome}</td>
@@ -70,3 +42,30 @@ export default function Produtos() {
     </div>
   )
 }
+
+//Exemplo de useEfect
+// useEffect(()=>{
+//   console.log("Este useEffect renderiza sempre! " + counter);
+// });
+
+// const [counter, setCounter] = useState(0);
+
+// const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
+
+// useEffect(()=>{
+//   console.log("Este useEffect renderiza apenas uma vez!");
+//   setListaProdutosLocal(ListaProdutos);
+// },[ ]);
+
+// const [counter2, setCounter2] = useState(0)
+
+// useEffect(()=>{
+//   console.log("Este useEffect renderiza sempre que o objeto/variável ou elemento que está sendo monitorado no array de dependências sofra alguma atualização.");
+// },[counter2]);
+
+// <div>
+// <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+// </div>
+// <div>
+// <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+// </div>
